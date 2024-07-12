2c0ddb734bf4f27513ec9e949e5731cf7e0bab28
dudco0040 <dudco0040@gmail.com>
2024-07-12 22:43:59 +0900

    7/12 Component Styling

---
+++ 03.Component/ex02/src/06/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+// import './assets/scss/App.scss';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+  text-align: center;
+`;
+
+function App() {
+    return (
+        <StyleDiv id={'App'}>
+           <Header />
+        </StyleDiv>
+    );
+}
+
+export default App;
